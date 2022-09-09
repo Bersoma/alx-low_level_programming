b1fc1f5352b0aaf42d6fa83d1b9823c7b6db58f2
Bersoma <uzoekwe_emmanuel@yahoo.com>
2022-09-08 19:33:29 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  *main - Entry point
- *
+ *	is printing programming is like building
  *   *Return: Always 0 (Success)
  *
  *    */
 
-
-
 int main(void)
 
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
