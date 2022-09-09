6b85bd2a606bcc536008999170ac6a059bd2193f
Bersoma <uzoekwe_emmanuel@yahoo.com>
2022-09-08 19:43:41 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 /**
- *
- *  *main - Entry point
- *	is printing programming is like building
- *   *Return: Always 0 (Success)
- *
- *    */
-
+* main - print the string in the put function
+*
+* Description: using the main function 
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
-
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
