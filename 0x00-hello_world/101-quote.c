fbd75151907b85f908b665859e1b617d1e9d8347
Bersoma <uzoekwe_emmanuel@yahoo.com>
2022-09-08 20:19:24 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - prints the main function
  *
  * Description: using the main function
- * this program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * this program prints "and that piece of art is useful" - Dora Korpar
  * Return: 0
  */
 int main(void)
