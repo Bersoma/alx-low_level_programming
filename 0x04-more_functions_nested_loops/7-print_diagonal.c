4b918c755125302d380327133d69b04091e36771
Bersoma <uzoekwe_emmanuel@yahoo.com>
2022-09-16 03:11:43 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,32 +1,25 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
 * print_diagonal - print character "\" n times
-* print_diagonal - print character "\" n times
+* @n: number of characters to draw
 */
 
 void print_diagonal(int n)
 {
-	int len, space;
+int draw;
+int space;
 
-	if (n > 0)
-	{
-		for (len = 0; len < n; len++)
-		{
-			for (space = 0; space < len; space++)
-			{
-				putchar(' ');
-			}
-
-			putchar('\\');
-
-			if (len == (n - 1))
-			{
-				continue;
-			}
-			putchar('\n');
-		}
-	}
-	putchar('\n');
+if (n > 0)
+{
+for (draw = 1; draw <= n; draw++)
+{
+for (space = 1; space < draw; space++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
 }
