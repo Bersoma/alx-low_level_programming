2e30b580427f9c729213c2dcb1f80d8b68278fdd
Bersoma <uzoekwe_emmanuel@yahoo.com>
2022-09-19 16:56:36 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+* _puts - prints a string, followed by a new line, to stdout
+* @str: string to print
+* Description: prints a string
+* On success: returns no error
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+		_putchar(*str++);
+
+	_putchar('\n');
+}
