07dd9e5acb5c494e2ba064460a661cd4a27f28ff
Bersoma <uzoekwe_emmanuel@yahoo.com>
2022-09-28 19:10:55 -0700

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,14 +3,14 @@
 /**
 * _strlen_recursion - return string length
 * @s: string
-* Return; string length
+* Return: string length
 */
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
+if (*s == '\0')
+return (0);
 
-	s++;
-	return (1 + _strlen_recursion(s));
+s++;
+return (1 + _strlen_recursion(s));
 }
