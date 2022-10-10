ba5fe78f9d3589096318cbab0745013a475bd936
Bersoma <uzoekwe_emmanuel@yahoo.com>
2022-10-10 07:48:40 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,9 @@
-#ifndef "DOG_H"
-#define "DOG_H"
+#ifndef DOG_H
+#define DOG_H
 
 /**
 * dog_t - Typedef for struct dog
 */
-
 typedef struct dog dog_t;
 
 /**
@@ -26,4 +25,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /*Endif DOG_H*/
