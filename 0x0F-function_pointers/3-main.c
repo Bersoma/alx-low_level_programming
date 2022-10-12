a57d0888b7e9530143e5930a0e3526a72bc3b9ea
Bersoma <uzoekwe_emmanuel@yahoo.com>
2022-10-12 07:03:41 -0700

    task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,46 +1,45 @@
-#include <stdio.h> /* printf */
-#include <stdlib.h> /* atoi */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
-* main - when user runs main,
-* user should give two integers and an operator and
-* main will calculate the math via a function pointer.
-* prints sum, difference, product, dividend, or remainder
-* @argc: argument counter
-* @argv: arguments
-* Return: 0 on sucess
+* main - a program that performs simple arithmetic operations by calling
+* helper functions
+* @argc: the number of command-line arguments
+* @argv: an array of strings containing one cmd-line argument per string
+* Return: returns 0 (success)
 */
 
 int main(int argc, char *argv[])
 {
-	int n1, n2;
-	int (*f)(int, int);
+	int num1, num2, result;
+	int (*ptr)(int, int);
 
-	/* validate input */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	/* convert user input to ints and point to correct operator function */
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	f = get_op_func(argv[2]);
-
-	if (f == NULL || (argv[2][1] != '\0'))
+	if (strlen(argv[2]) == 1 && (argv[2][0] == '+' || argv[2][0] == '-'
+				|| argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%'))
 	{
-		printf("Error\n");
-		exit(99);						
-	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
-	{
-		printf("Error\n");
-		exit(100);
-	}
+		if (atoi(argv[3]) == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+		{
+			printf("Error\n");
+			exit(100);
+		}
 
-	printf("%d\n", f(n1, n2)); /* calculate via function ptr */
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+		ptr = get_op_func(argv[2]);
+		result = ptr(num1, num2);
 
-	return (0);
+		printf("%d\n", result);
+
+		return (0);
+	}
+	printf("Error\n");
+	exit(99);
 }
